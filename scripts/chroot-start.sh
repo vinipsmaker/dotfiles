fbfabe6576fd74ed89c74d2f3e95fc6de7f39829
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-09-23 00:23:39 -0300

    Improved chroot script

--- scripts/chroot-start.sh
+++ scripts/chroot-start.sh
@@ -1,5 +1,17 @@
-mount -o bind /proc proc
-mount -o bind /dev dev
-mount -o bind /dev/pts dev/pts
-mount -o bind /dev/shm dev/shm
-mount -o bind /sys sys
+CHROOT_DIR="$(dirname $0)"
+
+if [ -n "$(ls ${CHROOT_DIR}/proc)" ]; then
+    echo "Chrooting..."
+    chroot "${CHROOT_DIR}"
+    exit
+fi
+
+echo "Mounting..."
+mount -o bind /proc "${CHROOT_DIR}/proc"
+mount -o bind /dev "${CHROOT_DIR}/dev"
+mount -o bind /dev/pts "${CHROOT_DIR}/dev/pts"
+mount -o bind /dev/shm "${CHROOT_DIR}/dev/shm"
+mount -o bind /sys "${CHROOT_DIR}/sys"
+
+echo "Chrooting..."
+chroot "${CHROOT_DIR}"
