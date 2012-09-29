021c813afc0aed6df5fd0323f574977cc761a4aa
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-09-29 12:19:26 -0300

    Improved chroot scripts

--- scripts/chroot-start.sh
+++ scripts/chroot-start.sh
@@ -1,17 +1,13 @@
 CHROOT_DIR="$(dirname $0)"
 
-if [ -n "$(ls ${CHROOT_DIR}/proc)" ]; then
-    echo "Chrooting..."
-    chroot "${CHROOT_DIR}"
-    exit
+if [ ! -e "${CHROOT_DIR}/proc/cpuinfo" ]; then
+    echo "Mounting..."
+    mount -o bind /proc "${CHROOT_DIR}/proc"
+    mount -o bind /dev "${CHROOT_DIR}/dev"
+    mount -o bind /dev/pts "${CHROOT_DIR}/dev/pts"
+    mount -o bind /dev/shm "${CHROOT_DIR}/dev/shm"
+    mount -o bind /sys "${CHROOT_DIR}/sys"
 fi
-
-echo "Mounting..."
-mount -o bind /proc "${CHROOT_DIR}/proc"
-mount -o bind /dev "${CHROOT_DIR}/dev"
-mount -o bind /dev/pts "${CHROOT_DIR}/dev/pts"
-mount -o bind /dev/shm "${CHROOT_DIR}/dev/shm"
-mount -o bind /sys "${CHROOT_DIR}/sys"
 
 echo "Chrooting..."
 chroot "${CHROOT_DIR}"
--- scripts/chroot-stop.sh
+++ scripts/chroot-stop.sh
@@ -1,5 +1,10 @@
-umount proc
-umount dev/pts
-umount dev/shm
-umount dev
-umount sys
+CHROOT_DIR="$(dirname $0)"
+
+if [ -e "${CHROOT_DIR}/proc/cpuinfo" ]; then
+    echo "Unmounting..."
+    umount "${CHROOT_DIR}/proc"
+    umount "${CHROOT_DIR}/dev/pts"
+    umount "${CHROOT_DIR}/dev/shm"
+    umount "${CHROOT_DIR}/dev"
+    umount "${CHROOT_DIR}/sys"
+fi
