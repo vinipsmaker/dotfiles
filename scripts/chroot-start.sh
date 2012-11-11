0e9cbba4a914ad1e2ca6dec928953040cf66871d
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2012-11-11 20:59:29 -0300

    security: no longer mounting /dev in chroot scripts

--- scripts/chroot-start.sh
+++ scripts/chroot-start.sh
@@ -3,10 +3,31 @@ CHROOT_DIR="$(dirname $0)"
 if [ ! -e "${CHROOT_DIR}/proc/cpuinfo" ]; then
     echo "Mounting..."
     mount -o bind /proc "${CHROOT_DIR}/proc"
-    mount -o bind /dev "${CHROOT_DIR}/dev"
-    mount -o bind /dev/pts "${CHROOT_DIR}/dev/pts"
-    mount -o bind /dev/shm "${CHROOT_DIR}/dev/shm"
+
+    # /dev/shm
+    # required for chromium and other programs
+    mkdir "${CHROOT_DIR}/dev/shm"
+    mount -t tmpfs none "${CHROOT_DIR}/dev/shm"
+
+    # tty and pts
+    # required for xterm and alike
+    > "${CHROOT_DIR}/dev/ptmx"
+    mount -o bind /dev/ptmx "${CHROOT_DIR}/dev/ptmx"
+    mkdir "${CHROOT_DIR}/dev/pts"
+    mount -t devpts none "${CHROOT_DIR}/dev/pts"
+
+    # /dev/{null,zero,random,urandom}
+    > "${CHROOT_DIR}/dev/null"
+    mount -o bind /dev/null "${CHROOT_DIR}/dev/null"
+    > "${CHROOT_DIR}/dev/zero"
+    mount -o bind /dev/zero "${CHROOT_DIR}/dev/zero"
+    > "${CHROOT_DIR}/dev/random"
+    mount -o bind /dev/random "${CHROOT_DIR}/dev/random"
+    > "${CHROOT_DIR}/dev/urandom"
+    mount -o bind /dev/urandom "${CHROOT_DIR}/dev/urandom"
+
     mount -o bind /sys "${CHROOT_DIR}/sys"
+    mount -o nosuid,nodev -t tmpfs none "${CHROOT_DIR}/tmp"
 fi
 
 echo "Chrooting..."
--- scripts/chroot-stop.sh
+++ scripts/chroot-stop.sh
@@ -3,8 +3,29 @@ CHROOT_DIR="$(dirname $0)"
 if [ -e "${CHROOT_DIR}/proc/cpuinfo" ]; then
     echo "Unmounting..."
     umount "${CHROOT_DIR}/proc"
-    umount "${CHROOT_DIR}/dev/pts"
-    umount "${CHROOT_DIR}/dev/shm"
-    umount "${CHROOT_DIR}/dev"
+
+    # /dev/shm
+    # required for chromium and other programs
+    umount -l "${CHROOT_DIR}/dev/shm"
+    rmdir "${CHROOT_DIR}/dev/shm"
+
+    # tty and pts
+    # required for xterm and alike
+    umount -l "${CHROOT_DIR}/dev/ptmx"
+    rm "${CHROOT_DIR}/dev/ptmx"
+    umount -l "${CHROOT_DIR}/dev/pts"
+    rmdir "${CHROOT_DIR}/dev/pts"
+
+    # /dev/{null,zero,random,urandom}
+    umount -l "${CHROOT_DIR}/dev/null"
+    rm "${CHROOT_DIR}/dev/null"
+    umount -l "${CHROOT_DIR}/dev/zero"
+    rm "${CHROOT_DIR}/dev/zero"
+    umount -l "${CHROOT_DIR}/dev/random"
+    rm "${CHROOT_DIR}/dev/random"
+    umount -l "${CHROOT_DIR}/dev/urandom"
+    rm "${CHROOT_DIR}/dev/urandom"
+
     umount "${CHROOT_DIR}/sys"
+    umount "${CHROOT_DIR}/tmp"
 fi
